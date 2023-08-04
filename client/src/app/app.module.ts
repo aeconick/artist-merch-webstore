2b4d941833c786eb0382bfc0a137146de13a0bf3
Nikolay Cherepov <aeconick@gmail.com>
2023-08-04 21:18:31 +0300

    Add input validation component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -17,6 +17,8 @@ import { CartPageComponent } from './components/pages/cart-page/cart-page.compon
 import { TitleComponent } from './components/partials/title/title.component';
 import { NotFoundComponent } from './components/partials/not-found/not-found.component';
 import { LoginPageComponent } from './components/pages/login-page/login-page.component';
+import { InputContainerComponent } from './components/partials/input-container/input-container.component';
+import { InputValidationComponent } from './components/partials/input-validation/input-validation.component';
 
 @NgModule({
   declarations: [
@@ -31,6 +33,8 @@ import { LoginPageComponent } from './components/pages/login-page/login-page.com
     TitleComponent,
     NotFoundComponent,
     LoginPageComponent,
+    InputContainerComponent,
+    InputValidationComponent,
   ],
   imports: [
     BrowserModule,
